f433d7a580604d5d773ef68fab860b80c84f5d8c
dongge-c-language <2296954740@qq.com>
2026-01-14 01:53:39 +0800

    角色相机与弹簧臂(Character Camera and Spring Arm)

--- Source/Slash_New/Private/Characters/SlashCharacter.cpp
+++ Source/Slash_New/Private/Characters/SlashCharacter.cpp
@@ -2,11 +2,27 @@
 
 
 #include "Characters/SlashCharacter.h"
+#include "GameFramework/SpringArmComponent.h"//弹簧臂头文件
+#include "Camera/CameraComponent.h"//摄像
 
 ASlashCharacter::ASlashCharacter()
 {
  	
 	PrimaryActorTick.bCanEverTick = true;
+
+	//用控制器旋转，让人物视角转向正常，人体不会随着“超人飞行”，而是站着换视角。
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	
+	//创造摇臂，附加到根组件上
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	//设置臂长
+	CameraBoom->TargetArmLength = 300.f;//由于设立了蓝图可见，所以蓝图也可以改。这个相当于默认值。
+
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCamera"));
+	ViewCamera->SetupAttachment(CameraBoom);//设置到CameraBoom目录里面
 
 }
 
@@ -30,7 +46,9 @@ void ASlashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 	//和鸟类似，绑定轴映射
 	PlayerInputComponent->BindAxis(FName("MoveForward"), this, &ASlashCharacter::MoveForward);
-
+	PlayerInputComponent->BindAxis(FName("MoveRight"), this, &ASlashCharacter::MoveRight);
+	PlayerInputComponent->BindAxis(FName("Turn"), this, &ASlashCharacter::Turn);
+	PlayerInputComponent->BindAxis(FName("LookUp"), this, &ASlashCharacter::LookUp);
 }
 
 void ASlashCharacter::MoveForward(float Value)
@@ -40,5 +58,24 @@ void ASlashCharacter::MoveForward(float Value)
 		FVector Forward = GetActorForwardVector();
 		AddMovementInput(Forward, Value);
 	}
+}
+
+void ASlashCharacter::MoveRight(float Value)
+{
+	if (Controller && (Value != 0.f))
+	{
+		FVector Right = GetActorRightVector();//调用右移函数，获得右移向量
+		AddMovementInput(Right, Value);
+	}
+}
+
+void ASlashCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void ASlashCharacter::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
 }
 
--- Source/Slash_New/Public/Characters/SlashCharacter.h
+++ Source/Slash_New/Public/Characters/SlashCharacter.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Character.h"
 #include "SlashCharacter.generated.h"
 
+class USpringArmComponent;//提前声明弹簧臂组件
+class UCameraComponent;//提前声明摄像机组件
+
 UCLASS()
 class SLASH_NEW_API ASlashCharacter : public ACharacter
 {
@@ -17,11 +20,17 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 protected:
-	void MoveForward(float Value); //前进函数。
-
-protected:
-	
 	virtual void BeginPlay() override;
+	void MoveForward(float Value); //前进函数。
+	void MoveRight(float Value);
+	void Turn(float Value);
+	void LookUp(float Value);
+private:
+	//添加弹簧臂组件
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* CameraBoom;
 
-
+	//添加摄像机组件
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* ViewCamera;
 };
